89e3c9c989d658b64214996f9bbcea569e94fe3c
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-05-09 22:01:09 +0600

    sidebar :art:

--- src/Components/Home/SideBar.js
+++ src/Components/Home/SideBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 
 const SideBar = () => {
   return (
@@ -78,3 +78,4 @@ const SideBar = () => {
 };
 
 export default SideBar;
+// Sidebar
