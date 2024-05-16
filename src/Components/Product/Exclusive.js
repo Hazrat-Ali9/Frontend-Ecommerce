8e110e9e53e2fc813e81b6d247901aa469d6d2dd
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-05-16 21:37:02 +0600

    Exclusive  Product :bug:

--- src/Components/Product/Exclusive.js
+++ src/Components/Product/Exclusive.js
@@ -4,7 +4,7 @@ import ExclusiveDetails from './Details/ExclusiveDetails';
 
 const Exclusive = () => {
     const [products] = useProduct();
-
+// Exclusive Product
   return (
     <div className="mx-5">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-12 gap-y-6">
