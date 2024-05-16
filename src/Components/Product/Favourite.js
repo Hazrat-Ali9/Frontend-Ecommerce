ab684c646ea7968929ac36f334efa01639dad9d8
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-05-16 21:38:13 +0600

    Favourite  Product :card_index:

--- src/Components/Product/Favourite.js
+++ src/Components/Product/Favourite.js
@@ -4,7 +4,7 @@ import FavouriteDetil from './Details/FavouriteDetil';
 
 const Favourite = () => {
     const [products] = useProduct();
-
+// Favourite Prodcut 
   return (
     <div className="mx-5">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-12 gap-y-6">
